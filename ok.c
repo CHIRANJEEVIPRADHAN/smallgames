22de1a0103d24a19a8b903f2842e47f4d80050c0
Chiranjeevi Pradhan <89828582+CHIRANJEEVIPRADHAN@users.noreply.github.com>
2023-09-17 11:08:57 +0530

    Add files via upload

---
+++ ok.c
@@ -0,0 +1,208 @@
+/*#include <stdio.h>
+int main(){
+    printf("hello world!!");
+    return 0;
+}*/
+
+/*
+#include <stdio.h>
+int main(){
+    printf("----------- WELCOME TO OUR LOAN ANALYZER ---------- \n");
+    float l,r,t,i,a;
+    printf("Enter the amount of loan taken :- ");
+    scanf("%f",&l);
+    printf("Enter the rate percent at which the loan is taken :- ");
+    scanf("%f",&r);
+    printf("Enter the due of loan :- ");
+    scanf("%f",&t);
+    i=(l*r*t)/100;
+    printf("The interest is %f \n",i);
+    a=l+i;
+    printf("The amount to be paid is %f \n",a);
+    printf("---------- THANK YOU FOR VISITING US ----------");
+    return 0;
+}
+*/
+
+//  **********PRACTICE SET 1**********
+// Q.1
+/*#include <stdio.h>
+int main(){
+    int l=3,b=8;
+    int a=l*b;
+    printf("the area of the rectangle is %d",a);
+    return 0 ;
+}*/
+// Q.2
+/*#include <stdio.h>
+int main(){
+    int radius = 3;
+    float pi = 3.14;
+    printf("The area of this circle is %f",pi*radius*radius);
+
+
+
+    return 0 ;
+}*/
+
+// CHAPTER 2
+/*#include <stdio.h>
+int main(){
+    int x=2;
+    int y=3;
+    printf("the value = %d\n",3*x-8*y);
+    return 0 ;
+}*/
+
+// CHAPTER 3
+/*#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a number \n");
+    scanf("%d",&a);
+    if (a%2==0){
+        printf("%d is even\n",a);
+    
+
+    }
+    else{
+        printf("%d is odd\n",a);
+    }
+*//*
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age \n");
+    scanf("%d",&age);
+    if (age>=70){
+        printf("you cannot drive.");
+
+    }
+    else{
+        printf("you can drive.");
+    }
+
+return 0 ;
+}*//*
+#include <stdio.h>
+int main()
+{
+    float income;
+    printf("Enter your family annual income :- ");
+    scanf("%d",&income);
+    if (income>=600000){
+        printf("Applicable for OBC(NCL) catagory.");
+
+    }
+    else{
+        printf("Not applicable for OBC(NCL)");
+    }
+     
+
+return 0;
+}
+*/
+
+//  expt.2
+//Q.1
+/*#include <stdio.h>
+int main()
+{
+    printf("name : chiranjeevi pradhan\n");
+    printf("dob : may 16,2004\n");
+    printf("mobile : 6656677889");
+    return 0;
+}*/
+//Q.2
+/*
+#include <stdio.h>
+int main()
+{
+    printf("#######\n");
+    printf("   #   \n");
+    printf("   #   \n");
+    printf("   #   \n");
+    printf("   #   \n");
+    printf("#######\n");
+    printf("       \n");
+    printf("       \n");
+    printf("########\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+
+    return 0 ;
+}*/
+//Q.3
+/*
+#include <stdio.h>
+int main()
+{
+    int a=23,b=44,add,sub;
+    float w=22.9,y=55.8,p_add,p_sub;
+    add=a+b;
+    sub=a-b;
+    p_add=w+y;
+    p_sub=w-y;
+    printf("%d \n",add);
+    printf("%d \n",sub);
+    printf("%.1f \n",p_add);
+    printf("%.1f \n",p_sub);
+    
+
+return 0 ;
+}*/
+
+
+//Q.4
+/*#include <stdio.h>
+int main()
+{
+    float ht1;
+    float ht2;
+    float tot;
+
+
+    printf("Enter the height of the first person : ");
+    scanf("%f",&ht1);
+
+
+    printf("Enter the height of the second person : ");
+    scanf("%f",&ht2);
+
+
+    tot=ht1+ht2;
+    printf("The total height is %f",tot);
+    return 0 ;
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
